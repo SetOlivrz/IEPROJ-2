e2f4dce1bac6ca322c332aed70ce0736505335e7
CesarManalac <cesar_manalac@dlsu.edu.ph>
2021-09-03 10:35:50 +0800

    Disabled hotbar, added a working script towards the changing of the image in the top right

---
+++ IEPROJ/Assets/Scripts/Item.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Item : MonoBehaviour
+{
+    Sprite gun;
+    Sprite knife;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gun = Resources.Load<Sprite>("Pistol");
+        knife = Resources.Load<Sprite>("Machete");
+        this.GetComponent<Image>().sprite = gun;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Debug.Log("Gun");
+            this.GetComponent<Image>().sprite = gun;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            this.GetComponent<Image>().sprite = knife;
+        }
+    }
+}
