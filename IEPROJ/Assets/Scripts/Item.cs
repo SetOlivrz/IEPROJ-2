7d8176c21a5705750ac4565e90e5a0643f40e682
CesarManalac <cesar_manalac@dlsu.edu.ph>
2021-09-06 13:59:52 +0800

    Updated tool selection

--- IEPROJ/Assets/Scripts/Item.cs
+++ IEPROJ/Assets/Scripts/Item.cs
@@ -6,19 +6,31 @@ public class Item : MonoBehaviour
 {
     Sprite gun;
     Sprite knife;
+    Sprite can;
+    Sprite hoe;
 
     // Start is called before the first frame update
     void Start()
     {
         gun = Resources.Load<Sprite>("Pistol");
         knife = Resources.Load<Sprite>("Machete");
+        can = Resources.Load<Sprite>("Watering Can");
+        hoe = Resources.Load<Sprite>("Hoe");
         this.GetComponent<Image>().sprite = gun;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha5))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            this.transform.GetComponent<Image>().sprite = hoe;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            this.transform.GetComponent<Image>().sprite = can;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             this.transform.GetComponent<Image>().sprite = gun;
         }
