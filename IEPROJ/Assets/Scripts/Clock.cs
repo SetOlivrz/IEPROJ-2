887df39e3f01e432e2fca06b4479b163779bc0c3
CesarManalac <cesar_manalac@dlsu.edu.ph>
2021-09-09 21:15:54 +0800

    added quit and instructions to the pause menu

---
+++ IEPROJ/Assets/Scripts/Clock.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class Clock : MonoBehaviour
+{
+    private Text clock;
+    private float timer;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
